ecce50ed283487a8b1882d2e6202a016c09406e8
diazzqlz <diazzqlz7@gmail.com>
2024-04-07 18:50:21 -0300

    more routes added

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -36,6 +36,25 @@ export async function registerForEvent(app: FastifyInstance) {
       if(attendeeFromEmail !== null) {
         throw new Error('this e-mail is already registered for this event.')
       }
+
+      const [event, amountOfAttendeesForEvent] = await Promise.all([
+        prisma.event.findUnique({
+          where: {
+            id: eventId
+          }
+        }),
+        
+        prisma.attendee.count({
+          where: {
+            eventId: eventId
+          }
+        })
+
+      ])
+
+      if(event?.maximumAttendees && amountOfAttendeesForEvent > event?.maximumAttendees) {
+        throw new Error('the maximum number of attendees for this event has been reached.')
+      }
       
       const attendee = await prisma.attendee.create({
         data: {
